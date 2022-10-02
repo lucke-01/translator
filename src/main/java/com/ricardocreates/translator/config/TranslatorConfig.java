a9fa6d8262f92099099b893b9191dd48da6589ed
Ricardo <ricardojara01@gmail.com>
2022-10-02 12:54:51 +0200

    added build configuration

--- src/main/java/com/ricardocreates/translator/config/TranslatorConfig.java
+++ src/main/java/com/ricardocreates/translator/config/TranslatorConfig.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.ricardocreates.translator.model.KeyValuePair;
 
 public class TranslatorConfig {
-    public static final String CONFIG_FILE_NAME = "config.properties";
+    public static final String CONFIG_FILE_NAME = "./config/config.properties";
     public static final String MICROSOFT_API_KEY = "microsoft";
     public static final KeyValuePair<String, String> MICROSOFT_API = new KeyValuePair<>("microsoft", "Microsoft");
     public static final List<KeyValuePair<String, String>> AVAILABLE_APIS = List.of(MICROSOFT_API);
