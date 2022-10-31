d2b5ac0cff4d7e98ad8b9fb86d1e97e8ada6f1b6
Ricardo <ricardojara01@gmail.com>
2022-10-31 17:15:30 +0100

    new tests

--- src/main/java/com/ricardocreates/translator/interpreter/microsoft/MicrosfotApiInterpreterService.java
+++ src/main/java/com/ricardocreates/translator/interpreter/microsoft/MicrosfotApiInterpreterService.java
@@ -3,6 +3,9 @@ package com.ricardocreates.translator.interpreter.microsoft;
 import java.io.IOException;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
@@ -24,17 +27,22 @@ import com.ricardocreates.translator.interpreter.microsoft.entity.Translation;
 import com.ricardocreates.translator.interpreter.model.Language;
 import com.ricardocreates.translator.util.StringsCustomUtil;
 
+@NoArgsConstructor
 public class MicrosfotApiInterpreterService implements InterpreterService {
-    private static final String API_VERSION = "?api-version=3.0";
-    private static final String BASIC_URL_API = "https://api.cognitive.microsofttranslator.com/";
-    private static final String LANGUAGES_URL_API = String.format("%s%s%s", BASIC_URL_API,"languages",API_VERSION);
-    
+    private final String apiVersion = "?api-version=3.0";
+    private String basicUrlApi = "https://api.cognitive.microsofttranslator.com/";
+
     private MicrosoftInterpreterMapper msInterpreterMapper = Mappers.getMapper(MicrosoftInterpreterMapper.class);
     private final UserConfig userConfig = TranslatorConfig.getUserConfig();
-    
+
+    public MicrosfotApiInterpreterService(String basicUrlApi) {
+        this.basicUrlApi = basicUrlApi;
+    }
+
     @Override
     public List<Language> getAvailableLanguages() {
-        HttpGet availableLanguagesGet = new HttpGet(LANGUAGES_URL_API);
+        String languagesURlApi = String.format("%s%s%s", basicUrlApi,"languages",apiVersion);
+        HttpGet availableLanguagesGet = new HttpGet(languagesURlApi);
 
         String responseAvailableLanguagesString = "";
         try (CloseableHttpResponse responseAvailableLanguagesGet = HttpClients.createDefault().execute(availableLanguagesGet)){
@@ -52,7 +60,7 @@ public class MicrosfotApiInterpreterService implements InterpreterService {
 
     @Override
     public String translate(String sourceLanguage, String destLanguage, String text) {
-        final String translateUrl = String.format("%stranslate%s&from=%s&to=%s",BASIC_URL_API, API_VERSION, sourceLanguage, destLanguage);
+        final String translateUrl = String.format("%stranslate%s&from=%s&to=%s",basicUrlApi, apiVersion, sourceLanguage, destLanguage);
         System.out.println(translateUrl);
         HttpPost translatePost = new HttpPost(translateUrl);
         final String jsonStringBody = String.format("[{\"text\": \"%s\"}]", text);
@@ -60,13 +68,12 @@ public class MicrosfotApiInterpreterService implements InterpreterService {
         HttpEntity stringEntity = new StringEntity(jsonStringBody, ContentType.APPLICATION_JSON);
         translatePost.setEntity(stringEntity);
         translatePost.addHeader("Content-Type", "application/json");
-        //TODO change it
+        
         translatePost.addHeader("Ocp-Apim-Subscription-Key", userConfig.getMicrosoftApiConfig().getOcpApimSubscriptionKey());
         translatePost.addHeader("Ocp-Apim-Subscription-Region", userConfig.getMicrosoftApiConfig().getOcpApimSubscriptionRegion());
-        
 
         String responseTranslateString = "";
-        try (CloseableHttpResponse responseTranslatePost = HttpClients.createDefault().execute(translatePost)){
+        try (CloseableHttpResponse responseTranslatePost = HttpClients.createDefault().execute(translatePost)) {
             responseTranslateString = EntityUtils.toString(responseTranslatePost.getEntity());
             //remove array response to be an object response
             responseTranslateString = StringsCustomUtil.jsonArrayStringToJsonObjectString(responseTranslateString);
@@ -75,7 +82,6 @@ public class MicrosfotApiInterpreterService implements InterpreterService {
         } catch (ParseException p) {
             throw new RuntimeException("unable to parse response from microsoft service");
         }
-        System.out.println("RESPONSE TEXT \n"+responseTranslateString);
         TranslateResponseEntity translateResponse = GsonUtil.getGson().fromJson(responseTranslateString, TranslateResponseEntity.class);
         Translation translation = translateResponse.getTranslations().get(0);
         return translation.getText();
--- src/test/java/com/ricardocreates/translator/interpreter/MicrosoftApiInterpreterIT.java
+++ src/test/java/com/ricardocreates/translator/interpreter/MicrosoftApiInterpreterIT.java
@@ -1,24 +1,31 @@
 package com.ricardocreates.translator.interpreter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.emptyOrNullString;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 import java.util.List;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.ricardocreates.translator.interpreter.microsoft.MicrosfotApiInterpreterService;
 import com.ricardocreates.translator.interpreter.model.Language;
+
 class MicrosoftApiInterpreterIT {
-    
-    private InterpreterService microsoftInterpreter = new MicrosfotApiInterpreterService();
-    
+    private MicrosfotApiInterpreterService microsoftInterpreter = new MicrosfotApiInterpreterService("http://localhost:8000/");
+    static WireMockServer wireMockServer;
+    @BeforeAll
+    static void setUp() {
+        //will look for src/test/resources/mappings/*.json stub files
+        wireMockServer = new WireMockServer( 8000);
+        wireMockServer.start();
+    }
+
     @Test
     void should_get_availableLanguage() {
         //GIVEN
-        // microsoft api rest working
+        //wiremock in src/test/resources/mappings/microsoft-languageapi.json
         //WHEN
         List<Language> languages = microsoftInterpreter.getAvailableLanguages();
         //THEN
@@ -27,5 +34,18 @@ class MicrosoftApiInterpreterIT {
         assertThat(languages.get(0).getAlfa2Code(), not(emptyOrNullString()));
         assertThat(languages.get(0).getName(), not(emptyOrNullString()));
         assertThat(languages.get(0).getNativeName(), not(emptyOrNullString()));
+    }
+    @Test
+    void should_translate_text() {
+        //GIVEN
+        //wiremock in src/test/resources/mappings/microsoft-languageapi.json
+        String sourceLanguage = "es";
+        String destLanguage = "en";
+        String text = "hola";
+        //WHEN
+        String translatedText = microsoftInterpreter.translate(sourceLanguage, destLanguage, text);
+        //THEN
+        assertThat(translatedText, notNullValue());
+        assertThat(translatedText, is(equalTo("hello")));
     }
 }
